cba1370b9eb1389f44df80db83fe7d4b988f8e25
edinsonjimenez-art <edinson.jimenez@tecsup.edu.pe>
2026-06-03 15:38:31 -0500

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://django-vehiculos-api.onrender.com/api",
+  baseURL: "https://django-vehiculos-api.onrender.com/api/",
 });
 
 export default api;
